2f544760d7088b60051ab299e0ff1a1ab976eca3
Joel Mwaga <mwagahjoel14@gmail.com>
2025-05-15 12:48:00 +0300

    learnt sequences in python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    else:
+        fibonacci = [0, 1]
+        for _ in range(2, length):
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+        print(fibonacci[:length])
